d1e41cd22b45e0bcfc51c32e8e149272bcf45f3a
gurudanito001 <gurudanito001@gmail.com>
2020-11-09 13:31:31 +0100

    implemented async and await in UserController.js

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,30 +1,62 @@
 const User = require("./../services/UserService");
 
 function UserController() {
-  const listUsers = function(req, res) {
-    User.list().then(data => res.json(data));
+  const listUsers = async function(req, res) {
+    let data = await User.list();
+    if(data.err){
+      res.json(`err: ${data.err}`)
+    }else{
+      res.json(data)
+    }
   };
 
-  const addUser = function(req, res) {
-    User.add(req.body).then(data => res.json(data));
+  const addUser = async function(req, res) {
+    let data = await  User.add(req.body);
+    if(data){
+      res.json(data)
+    }else{
+      res.json("Error")
+    }
+    //User.add(req.body).then(data => res.json(data));
   };
 
-  const deleteUser = function(req, res) {
-    User.delete(req.params.id).then(() => res.json("User Account Deleted"));
+  const deleteUser = async function(req, res) {
+    let data = await  User.delete(req.params.id);
+    if(data){
+      res.json("User Account Deleted")
+    }else{
+      res.json("Error")
+    }
+    //User.delete(req.params.id).then((data) => res.json(data));
   };
 
-  const updateUser = function(req, res) {
-    User.update(req.params.id, req.body).then(data => res.json(data));
+  const updateUser = async function(req, res) {
+    let data = await User.update(req.params.id, req.body);
+    if(data){
+      res.json(data)
+    }else{
+      res.json("Error")
+    }
+    //User.update(req.params.id, req.body).then(data => res.json(data));
   }
 
-  const getUser = function(req, res) {
-    User.getUser(req.params.id).then(data => res.json(data)) 
+  const getUser = async function(req, res) {
+    let data = await User.getUser(req.params.id);
+    if(data){
+      res.json(data)
+    }else{
+      res.json("Error")
+    }
+    //User.getUser(req.params.id).then(data => res.json(data)) 
   }
 
-  const findEmail = function(req, res) {
-      User.findEmail(req.params.email).then(data =>{
+  const findEmail = async function(req, res) {
+    let data = await User.findEmail(req.params.email);
+    data ? res.json(true) : res.json(false)
+    
+      /* User.findEmail(req.params.email).then(data =>{
         data ? res.json(true) : res.json(false)
-      })
+      }) */
   }
 
   return {
